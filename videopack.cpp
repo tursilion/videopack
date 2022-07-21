c4c3a03857be175a6f6e426ee29fc597a48169a5
Tursi <tacobell@harmlesslion.com>
2022-07-20 23:53:42 -0600

    - added mono mode

--- videopack.cpp
+++ videopack.cpp
@@ -11,8 +11,8 @@
 #include <atlstr.h>
 #include <stdio.h>
 
-// define for no color images, double-rate mono data
-//#define MONO
+// when true, monochrome video and no color table
+bool MONO = false;
 
 // storage for TI bitmap data
 char workbuf1[6144];
@@ -22,8 +22,10 @@ char oldc[6144];
 
 int main(int argc, char* argv[])
 {
+    printf("videopack 20220720\n");
+
 	if (argc < 4) {
-		printf("videopack <ti bitmap prefix (5 digits appended)> <audio bin> <output file>\n");
+		printf("videopack <ti bitmap prefix (5 digits appended)> <audio bin> <output file> [-mono]\n");
 		return -1;
 	}
 
@@ -37,6 +39,13 @@ int main(int argc, char* argv[])
 		printf("can't open audio file\n");
 		return -1;
 	}
+
+    if (argc > 4) {
+        if (0 == strcmp(argv[4],"-mono")) {
+            MONO = true;
+            printf("Doing monochrome video...\n");
+        }
+    }
 
 	CString path = argv[1];
 
@@ -61,89 +70,90 @@ int main(int argc, char* argv[])
 		}
 		fclose(fp);
 
-#ifndef MONO
-		test=test.Left(test.GetLength()-1);
-		test+='c';
-		fp = fopen(test.GetString(), "rb");
-		if (NULL == fp) {
-			printf("\nCan't open color file %s\n", test.GetString());
-			return -1;
-		}
-		// found a file - the color image is expected to also be present
-		if (6144 != fread(workbuf2, 1, 6144, fp)) {
-			printf("\nBad read on color file %s\n", test.GetString());
-			return -1;
-		}
-		fclose(fp);
+        if (!MONO) {
+		    test=test.Left(test.GetLength()-1);
+		    test+='c';
+		    fp = fopen(test.GetString(), "rb");
+		    if (NULL == fp) {
+			    printf("\nCan't open color file %s\n", test.GetString());
+			    return -1;
+		    }
+		    // found a file - the color image is expected to also be present
+		    if (6144 != fread(workbuf2, 1, 6144, fp)) {
+			    printf("\nBad read on color file %s\n", test.GetString());
+			    return -1;
+		    }
+		    fclose(fp);
 
-#if 0
-		// This looks worse!
+    #if 0
+		    // This looks worse!
+            // TODO: but isn't this what the despeckle tool does??
 
-		// first fixup phase - we check colors against the previous frame. The converter
-		// always tries to put the most populous color in the foreground. In some cases
-		// (especially black and white), movement could cause the color palette to swap
-		// unnecessarily, causing bigger pixel jumps than needed. So, all we are doing
-		// here is checking - if a color palette matches the previous frame's palette,
-		// swap it so that the colors don't change if they don't need to. This should
-		// reduce visible artifacts in those cases.
-		for (int idx=0; idx<6144; idx++) {
-			bool swapneeded = false;
-			// if foreground color matches, we don't need to check it any further
-			if ((workbuf2[idx] & 0xf0) != (oldc[idx]&0xf0)) {
-				// does not match - does it match the background color?
-				if ((workbuf2[idx] & 0xf0) == ((oldc[idx]&0x0f)<<4)) {
-					// yes, do the swap
-					swapneeded = true;
-				}
-#if 0
-				// looks awful with background swaps
-				else {
-					// check background (only if foreground didn't already match)
-					if ((workbuf2[idx]&0x0f) != (oldc[idx]&0x0f)) {
-						// does not match, does it match foreground?
-						if ((workbuf2[idx]&0x0f) == ((oldc[idx]&0xf0)>>4)) {
-							// yes, do the swap
-							swapneeded = true;
-						}
-					}
-				}
-#endif
-			}
-			if (swapneeded) {
-				// swap colors and pixels to better match the previous frame
-				workbuf2[idx] = ((workbuf2[idx]&0xf0)>>4) | ((workbuf2[idx]&0x0f)<<4);
-				workbuf1[idx] = ~workbuf1[idx];
-			}
-		}
-#endif
+		    // first fixup phase - we check colors against the previous frame. The converter
+		    // always tries to put the most populous color in the foreground. In some cases
+		    // (especially black and white), movement could cause the color palette to swap
+		    // unnecessarily, causing bigger pixel jumps than needed. So, all we are doing
+		    // here is checking - if a color palette matches the previous frame's palette,
+		    // swap it so that the colors don't change if they don't need to. This should
+		    // reduce visible artifacts in those cases.
+		    for (int idx=0; idx<6144; idx++) {
+			    bool swapneeded = false;
+			    // if foreground color matches, we don't need to check it any further
+			    if ((workbuf2[idx] & 0xf0) != (oldc[idx]&0xf0)) {
+				    // does not match - does it match the background color?
+				    if ((workbuf2[idx] & 0xf0) == ((oldc[idx]&0x0f)<<4)) {
+					    // yes, do the swap
+					    swapneeded = true;
+				    }
+    #if 0
+				    // looks awful with background swaps
+				    else {
+					    // check background (only if foreground didn't already match)
+					    if ((workbuf2[idx]&0x0f) != (oldc[idx]&0x0f)) {
+						    // does not match, does it match foreground?
+						    if ((workbuf2[idx]&0x0f) == ((oldc[idx]&0xf0)>>4)) {
+							    // yes, do the swap
+							    swapneeded = true;
+						    }
+					    }
+				    }
+    #endif
+			    }
+			    if (swapneeded) {
+				    // swap colors and pixels to better match the previous frame
+				    workbuf2[idx] = ((workbuf2[idx]&0xf0)>>4) | ((workbuf2[idx]&0x0f)<<4);
+				    workbuf1[idx] = ~workbuf1[idx];
+			    }
+		    }
+    #endif
 
-		// but this one helps a lot
-		// second quick fixup phase -- the image converter has a habit of using black
-		// for unused colors (ie: solid pattern) - this causes black sparkles in animation
-		// So, check and fixup - if we find a pattern byte that is either 0xff or 0x00,
-		// set it to 0x00 (no pixels set) and set the color foreground and background
-		// to be the same. When the new pixels are set, nothing should change till the color
-		// table is updated, which is second, reducing sparkles.
-		// TODO: we could do one better and use lookahead for any solid pattern to pre-
-		// select the unused color to something useful - but I guess not really useful, it
-		// won't visibly change till the color is updated anyway.
-		for (int idx=0; idx<6144; idx++) {
-			if (workbuf1[idx] == 0) {
-				// just a background color, so fixup the color table
-				workbuf2[idx] = workbuf2[idx] & 0x0f;
-				workbuf2[idx] |= workbuf2[idx]<<4;
-			} else if (workbuf1[idx] == 0xff) {
-				// just a foreground color - make background and fixup
-				workbuf1[idx] = 0;
-				workbuf2[idx] = (workbuf2[idx] & 0xf0)>>4;
-				workbuf2[idx] |= workbuf2[idx]<<4;
-			}
-		}
+		    // but this one helps a lot
+		    // second quick fixup phase -- the image converter has a habit of using black
+		    // for unused colors (ie: solid pattern) - this causes black sparkles in animation
+		    // So, check and fixup - if we find a pattern byte that is either 0xff or 0x00,
+		    // set it to 0x00 (no pixels set) and set the color foreground and background
+		    // to be the same. When the new pixels are set, nothing should change till the color
+		    // table is updated, which is second, reducing sparkles.
+		    // TODO: we could do one better and use lookahead for any solid pattern to pre-
+		    // select the unused color to something useful - but I guess not really useful, it
+		    // won't visibly change till the color is updated anyway.
+		    for (int idx=0; idx<6144; idx++) {
+			    if (workbuf1[idx] == 0) {
+				    // just a background color, so fixup the color table
+				    workbuf2[idx] = workbuf2[idx] & 0x0f;
+				    workbuf2[idx] |= workbuf2[idx]<<4;
+			    } else if (workbuf1[idx] == 0xff) {
+				    // just a foreground color - make background and fixup
+				    workbuf1[idx] = 0;
+				    workbuf2[idx] = (workbuf2[idx] & 0xf0)>>4;
+				    workbuf2[idx] |= workbuf2[idx]<<4;
+			    }
+		    }
 
-		// save the results
-		memcpy(oldp, workbuf1, sizeof(oldp));
-		memcpy(oldc, workbuf2, sizeof(oldc));
-#endif
+		    // save the results
+		    memcpy(oldp, workbuf1, sizeof(oldp));
+		    memcpy(oldc, workbuf2, sizeof(oldc));
+        }   // !mono
 
 		// we want to read the first 16 rows, and the first 24 characters of each row
 		// files 1 and 2 pretty much track, but it's easier with separate counters
@@ -209,24 +219,24 @@ int main(int argc, char* argv[])
 			}
 		}
 
-#ifndef MONO
-		fputc(fgetc(faudio), fout);		// audio byte
+        if (!MONO) {
+		    fputc(fgetc(faudio), fout);		// audio byte
 
-		for (int idx=0; idx<192; idx++) {
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(fgetc(faudio), fout);		// audio byte
+		    for (int idx=0; idx<192; idx++) {
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(fgetc(faudio), fout);		// audio byte
 
-			// every 192 bytes in the video file, we need to jump to the next line (24 columns)
-			// That's an addition of 8 columns, or 64 bytes.
-			if (filepos2 == jumppos2) {
-				filepos2+=64;
-				jumppos2=filepos2+192;
-			}
-		}
-#endif
+			    // every 192 bytes in the video file, we need to jump to the next line (24 columns)
+			    // That's an addition of 8 columns, or 64 bytes.
+			    if (filepos2 == jumppos2) {
+				    filepos2+=64;
+				    jumppos2=filepos2+192;
+			    }
+		    }
+        }
 
 		// second third part 1
 		fputc(fgetc(faudio), fout);		// audio byte
@@ -246,24 +256,24 @@ int main(int argc, char* argv[])
 			}
 		}
 
-#ifndef MONO
-		fputc(fgetc(faudio), fout);		// audio byte
+        if (!MONO) {
+		    fputc(fgetc(faudio), fout);		// audio byte
 
-		for (int idx=0; idx<192; idx++) {
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(fgetc(faudio), fout);		// audio byte
+		    for (int idx=0; idx<192; idx++) {
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(fgetc(faudio), fout);		// audio byte
 
-			// every 192 bytes in the video file, we need to jump to the next line (24 columns)
-			// That's an addition of 8 columns, or 64 bytes.
-			if (filepos2 == jumppos2) {
-				filepos2+=64;
-				jumppos2=filepos2+192;
-			}
-		}
-#endif
+			    // every 192 bytes in the video file, we need to jump to the next line (24 columns)
+			    // That's an addition of 8 columns, or 64 bytes.
+			    if (filepos2 == jumppos2) {
+				    filepos2+=64;
+				    jumppos2=filepos2+192;
+			    }
+		    }
+        }
 
 		// second third part 2
 		fputc(fgetc(faudio), fout);		// audio byte
@@ -283,24 +293,24 @@ int main(int argc, char* argv[])
 			}
 		}
 
-#ifndef MONO
-		fputc(fgetc(faudio), fout);		// audio byte
+        if (!MONO) {
+		    fputc(fgetc(faudio), fout);		// audio byte
 
-		for (int idx=0; idx<192; idx++) {
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(fgetc(faudio), fout);		// audio byte
+		    for (int idx=0; idx<192; idx++) {
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(fgetc(faudio), fout);		// audio byte
 
-			// every 192 bytes in the video file, we need to jump to the next line (24 columns)
-			// That's an addition of 8 columns, or 64 bytes.
-			if (filepos2 == jumppos2) {
-				filepos2+=64;
-				jumppos2=filepos2+192;
-			}
-		}
-#endif
+			    // every 192 bytes in the video file, we need to jump to the next line (24 columns)
+			    // That's an addition of 8 columns, or 64 bytes.
+			    if (filepos2 == jumppos2) {
+				    filepos2+=64;
+				    jumppos2=filepos2+192;
+			    }
+		    }
+        }
 
 		// third third
 		fputc(fgetc(faudio), fout);		// audio byte
@@ -320,24 +330,24 @@ int main(int argc, char* argv[])
 			}
 		}
 
-#ifndef MONO
-		fputc(fgetc(faudio), fout);		// audio byte
+        if (!MONO) {
+		    fputc(fgetc(faudio), fout);		// audio byte
 
-		for (int idx=0; idx<192; idx++) {
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(workbuf2[filepos2++], fout);
-			fputc(fgetc(faudio), fout);		// audio byte
+		    for (int idx=0; idx<192; idx++) {
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(workbuf2[filepos2++], fout);
+			    fputc(fgetc(faudio), fout);		// audio byte
 
-			// every 192 bytes in the video file, we need to jump to the next line (24 columns)
-			// That's an addition of 8 columns, or 64 bytes.
-			if (filepos2 == jumppos2) {
-				filepos2+=64;
-				jumppos2=filepos2+192;
-			}
-		}
-#endif
+			    // every 192 bytes in the video file, we need to jump to the next line (24 columns)
+			    // That's an addition of 8 columns, or 64 bytes.
+			    if (filepos2 == jumppos2) {
+				    filepos2+=64;
+				    jumppos2=filepos2+192;
+			    }
+		    }
+        }
 
 	}
 
